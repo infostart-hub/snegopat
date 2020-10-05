4ccef5aceab9690bdda536b3c7b38b09b06e73f2
tormozit <tormozit@gmail.com>
2020-10-05 08:18:23 +0300

    Revert "сортировка списка переходов"
    This reverts commit 4c41bdac2aace2a2ecd25ef8ab07433aac4f4555.

--- addins/jump2refs.js
+++ addins/jump2refs.js
@@ -111,8 +111,6 @@ function doJump(command, forceShow)
     {
         refs.splice(0, 1)
         var dlg = new SelectValueDialog("Выберите объект для перехода!", refs);
-        dlg.form.SortByName = true; // tormozit
-        dlg.sortValuesList(true); // tormozit
         if (dlg.selectValue())
             choice = dlg.selectedValue;
         else
